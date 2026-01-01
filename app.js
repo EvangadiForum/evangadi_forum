243ba84481c583144cab0d6448cb531233a25517
👑┃MK <contact.mkykg@gmail.com>
2026-01-01 10:53:48 -0600

    i got backend support doing the frontend

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3307;
 
+// const dbConnection = require("./db/dbConfig")
+
 const userRoute = require("./routes/userRoute")
 app.use("/api/users", userRoute)
 
+
+// try {
+    
+// } catch (error) {
+    
+// }
 app.listen(port, (err) => {
     if (err) {
         console.log(err.message)
