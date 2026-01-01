3111a3c3a7748fb07b4a60651d242a6122d2f07c
👑┃MK <contact.mkykg@gmail.com>
2026-01-01 11:13:28 -0600

    react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import Dashboard from './pages/dashboard/Dashboard'
+import Login from './pages/login/Login'
+import Register from './pages/register/Register'
+import { Routes, Route } from 'react-router-dom'
 function App() {
   return (
-    <div>App</div>
+    <>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </>
   )
 }
 
