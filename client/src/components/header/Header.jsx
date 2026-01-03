c0e1cf3b78dd8baf11bac83a01a6c1a469b3c204
👑┃MK <contact.mkykg@gmail.com>
2026-01-03 17:43:55 -0600

    header + header routing and env example

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import module from "./header.module.css";
+import evangadi from '../../../public/evangadi.png'
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+      <>
+          <div className={module.header}>
+              <div className={module.logo}>
+                  <img src={evangadi} alt="" />
+              </div>
+
+              <div className={module.text}>
+                  <Link to="#" style={{textDecoration: "none", color: "rgba(0,0,0,.55)"}}>
+                      <p>Home</p>
+                  </Link>
+                  <Link to="#" style={{ textDecoration: "none", color: "rgba(0,0,0,.55)", marginRight: "20px"}}>
+                      <p>How it Works</p>
+                  </Link>
+              </div>
+
+              <div className={module.btn}>
+                  <Link to="/login">
+                  <button>SIGN IN</button>
+                  </Link>
+              </div>
+          </div>
+      </>
+  )
+}
+
+export default Header
