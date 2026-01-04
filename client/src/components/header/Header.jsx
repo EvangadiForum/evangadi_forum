2863a1ef9df0fd7cc20ca2208acbfce5610d5462
👑┃MK <contact.mkykg@gmail.com>
2026-01-03 20:01:40 -0600

    Footer ig its finally done

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import module from "./header.module.css";
+import module from "./header.module.css"
 import evangadi from '../../../public/evangadi.png'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
