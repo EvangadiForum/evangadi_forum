6286c983e34b544fc97efd2408ed2bd18f256c27
Efama <dhifamahaile07@gmail.com>
2026-01-04 18:46:38 +0300

    Signup UI, Form validation, and API Integration.

---
+++ client/frontend_evangadi_forum/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import Sign_up from "./Components/Sign_up/Sign_up";
+import Router from "./Router";
+
+function App() {
+  return (
+    <>
+      <Router />
+    </>
+  );
+}
+
+export default App;
